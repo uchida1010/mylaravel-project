79e1261b0bf15a10a4746f0a9720460adfc53022
uchida1010 <tomoki.ksu.0826@gmail.com>
2023-02-09 22:18:36 +0900

    controller exit

--- app/Http/Controllers/ScrapingShowController.php
+++ app/Http/Controllers/ScrapingShowController.php
@@ -9,6 +9,40 @@ class ScrapingShowController extends Controller
     public function show()
     {
         $message = '競合調査';
-        return view('scraping.show', compact('message'));
+
+        $category_date = [
+            'plasticpallet' => 'プラスチックパレット',
+            'woodenpallet' => '木製パレット',
+        ];
+
+        $size_date = [
+            '1000size' => '1000サイズ以下',
+            '1100size' => '1100サイズ',
+            '1200size' => '1200サイズ',
+            '1300size' => '1300サイズ',
+            '1400size' => '1400サイズ',
+            '1500size' => '1500サイズ以上'
+        ];
+
+        $specification_date = [
+            'no' => 'ー',
+            'oneside' => '片面',
+            'bothsides' => '両面',
+            'mix' => '混在'
+        ];
+
+        $region_date = [
+            'no' => 'ー',
+            '北海道' => '北海道',
+            '東北' => '東北',
+            '関東' => '関東',
+            '中部' => '中部',
+            '近畿' => '近畿',
+            '中四国' => '中四国',
+            '九州' => '九州',
+            '沖縄' => '沖縄'
+        ];
+
+        return view('scraping.show', compact('message', 'category_date', 'size_date', 'specification_date', 'region_date'));
     }
 }
--- resources/views/scraping/show.blade.php
+++ resources/views/scraping/show.blade.php
@@ -20,12 +20,6 @@
                         カテゴリ
                     </td>
                     <td>
-                        @php
-                        $category_date = [
-                        'plastic_pallet' => 'プラスチックパレット',
-                        'wood_pallet' => '木製パレット',
-                        ];
-                        @endphp
                         <select name='category' class='category'>
                             @foreach ($category_date as $category_date_key => $category_date_val)
                             <option value=$category_date_key>{{$category_date_val}}</option>";
@@ -38,19 +32,9 @@
                         サイズ
                     </td>
                     <td>
-                        @php
-                        $size_date = [
-                        '1000size' => '1000サイズ以下',
-                        '1100size' => '1100サイズ',
-                        '1200size' => '1200サイズ',
-                        '1300size' => '1300サイズ',
-                        '1400size' => '1400サイズ',
-                        '1500size' => '1500サイズ以上'
-                        ];
-                        @endphp
                         <select name='size' class='size'>
                             @foreach ($size_date as $size_date_key => $size_date_val)
-                            <option value=$size_date_key>{{$size_date_val}}</option>";
+                            <option value=$size_date_key}}>{{$size_date_val}}</option>
                             @endforeach
                         </select>
 
@@ -61,14 +45,6 @@
                         仕様
                     </td>
                     <td>
-                        @php
-                        $specification_date = [
-                        'no' => 'ー',
-                        'oneside' => '片面',
-                        'bothsides' => '両面',
-                        'mix' => '混在'
-                        ];
-                        @endphp
                         <select name='specification' class='specification'>
                             @foreach ($specification_date as $specification_date_key => $specification_val)
                             <option value=$specification_date_key>{{$specification_val}}</option>";
@@ -81,19 +57,6 @@
                         置場(地方)
                     </td>
                     <td>
-                        @php
-                        $region_date = [
-                        'no' => 'ー',
-                        '北海道' => '北海道',
-                        '東北' => '東北',
-                        '関東' => '関東',
-                        '中部' => '中部',
-                        '近畿' => '近畿',
-                        '中四国' => '中四国',
-                        '九州' => '九州',
-                        '沖縄' => '沖縄'
-                        ];
-                        @endphp
                         <select name='region' class='region'>
                             @foreach ($region_date as $region_date_key => $region_date_val)
                             <option value=$region_date_key>{{$region_date_val}}</option>";
