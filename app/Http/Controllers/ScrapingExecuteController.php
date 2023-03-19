e1c2f6f56e84bfc40cf2c109ed7e73f071d37dba
uchida1010 <tomoki.ksu.0826@gmail.com>
2023-03-19 16:37:26 +0900

    url access

--- app/Http/Controllers/ScrapingExecuteController.php
+++ app/Http/Controllers/ScrapingExecuteController.php
@@ -6,19 +6,33 @@ use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PHPHtmlParser\Dom;
 
 class ScrapingExecuteController extends Controller
 {
   public function execute()
   {
 
+    $dom = new Dom;
+    $dom->loadFromUrl('https://www.pallet-o.com/');
+    $o_html = $dom->outerHtml;
+
+    $dom->loadFromUrl('https://www.reuse-pallet.com/');
+    $reuse_html = $dom->outerHtml;
+
+    $dom->loadFromUrl('https://sell.uppc.jp/');
+    $sell_html = $dom->outerHtml;
+
     // Spreadsheetオブジェクト生成
     $objSpreadsheet = new Spreadsheet();
     // シートの取得
     $objSheet = $objSpreadsheet->getActiveSheet();
 
-    // [A1]セルに文字列設定
-    $objSheet->setCellValue('A1', 'Spreadsheet');
+    //セルに文字列設定
+    $objSheet->setCellValue('A1', $o_html);
+    $objSheet->setCellValue('A2', $reuse_html);
+    $objSheet->setCellValue('A3', $sell_html);
+
 
     // ブラウザへの指定
     header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
